79bc1815385ad03b1a00149619fc5f4d016e5ffa
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-08-16 09:12:35 -0300

    adding struture examples

--- streams-filters-example/src/main/java/com/example/Main.java
+++ streams-filters-example/src/main/java/com/example/Main.java
@@ -5,16 +5,40 @@ public class Main {
     public static void main( String[] args ){
 
         System.out.println( "InputStream:" );
+        InputStreamExample.example();
 
-
+        
         System.out.println( "OutputStream:" );
+        OutputStreamExample.example();
 
-
+        
         System.out.println( "FilterStream:" );
+        FilterStreamExample.example();
         
         
         System.out.println( "BufferStream:" );
-
+        BufferStreamExample.example();
+        
+        
+        System.out.println( "InputStreamReader:" );
+        InputStreamReaderExample.example();
+        
+        
+        System.out.println( "OutputStreamWriter:" );
+        OutputStreamWriterExample.example();
+        
+        
+        System.out.println( "FilterReader:" );
+        FilterReadersExample.example();
+        
+        
+        System.out.println( "FilterWriters:" );
+        FilterWritersExample.example();
+        
+        
+        
+        System.out.println( "PrintWriter:" );
+        PrintWriterExample.example();
         
         
     }
