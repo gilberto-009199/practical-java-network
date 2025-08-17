31fee6186eea15358bdb06ad90793f1e9bd3a577
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-08-17 18:10:14 -0300

    example Thread simples

--- threads-example/src/main/java/com/example/Main.java
+++ threads-example/src/main/java/com/example/Main.java
@@ -3,7 +3,20 @@ package com.example;
 public class Main {
 
     public static void main( String[] args ){
-        
+
+        // ThreadsAndRunnableExample
+        new ThreadsAndRunnableExample().example();
+
+        // ReturningProcessThreadExample
+
+        // SyncronizationThreadExample
+
+        // DeadLockThreadExample
+
+        // SchedulingThreadExample
+
+        // PoolsAndExecutorsThreadExample
+
     }
 
 }
