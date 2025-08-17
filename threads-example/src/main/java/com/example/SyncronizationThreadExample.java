3e251f9471b3e8932be8d20f3790a6355bbf0323
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-08-17 19:00:02 -0300

    define examples for sync

--- threads-example/src/main/java/com/example/SyncronizationThreadExample.java
+++ threads-example/src/main/java/com/example/SyncronizationThreadExample.java
@@ -5,5 +5,12 @@ public class SyncronizationThreadExample implements Example{
     @Override
     public void example() {
 
+        // 1. Synchronized Blocks
+
+        // 2. Synchronized Methods
+
+        // 3. Alternatives to Synchronization
+        // + variáveis locais
+        // + java.util.concurrent.atomic.*
     }
 }
